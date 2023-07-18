17c88f9bbee6bdc716b8cd5ca508c4230557d458
jdm79 <james.malvern@gmail.com>
2023-07-18 19:15:11 +0100

    add custom queries

--- wp-content/themes/fictional-university-there/header.php
+++ wp-content/themes/fictional-university-there/header.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
-<html lang="en">
+<html <?php language_attributes()?>>
 <head>
   <?php wp_head(); ?>
-  <meta charset="UTF-8">
+  <meta charset="<?php bloginfo('charset')?>">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
-<body>
+<body <?php body_class()?>>
 <header class="site-header">
       <div class="container">
         <h1 class="school-logo-text float-left">
@@ -15,12 +15,15 @@
         <i class="site-header__menu-trigger fa fa-bars" aria-hidden="true"></i>
         <div class="site-header__menu group">
           <nav class="main-navigation">
+            <!-- <?php wp_nav_menu(array(
+              'theme_location' => 'headerMenuLocation'
+            ));?> -->
             <ul>
-              <li><a href="<?php echo site_url('/about-us') ?>">About Us</a></li>
+              <li <?php if(is_page('about-us') or wp_get_post_parent_id(0) == 17) echo 'class="current-menu-item"' ?>><a href="<?php echo site_url('/about-us') ?>">About Us</a></li>
               <li><a href="#">Programs</a></li>
-              <li><a href="#">Events</a></li>
+              <li <?php if(get_post_type() == 'event') echo 'class="current-menu-item"' ?>><a href="<?php echo get_post_type_archive_link('event'); ?>">Events</a></li>
               <li><a href="#">Campuses</a></li>
-              <li><a href="#">Blog</a></li>
+              <li <?php if(get_post_type() == 'post') echo 'class="current-menu-item"' ?>><a href="<?php echo site_url('/blog'); ?>">Blog</a></li>
             </ul>
           </nav>
           <div class="site-header__util">
